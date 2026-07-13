95a959748f8535d00cbe6c1320b07b3adbbe93f1
Alisher Orynbek <alisher.orynbek@tum.de>
2026-07-13 17:13:16 +0200

    Add SD card chip select pin definition
    Added SD_CS_Pin definition for SD card chip select.

--- firmware-stm32/Core/Inc/main.h
+++ firmware-stm32/Core/Inc/main.h
@@ -63,8 +63,8 @@ void Error_Handler(void);
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
-#define LD2_Pin GPIO_PIN_5
-#define LD2_GPIO_Port GPIOA
+#define SD_CS_Pin GPIO_PIN_1
+#define SD_CS_GPIO_Port GPIOB
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
